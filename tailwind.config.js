16cce0d654b315dd46f88d894cd37876bf2b02fa
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-05 14:37:25 +0100

    added extention for custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Jost']
+      }, colors: {
+        indigo: {
+          950: '#4346E7',
+          1000: '#8E8FE0',
+        }, neutral: {
+          950: '#121212',
+          1000: '#D9D9D9'
+        }
+      }
+    },
   },
   plugins: [],
 }
