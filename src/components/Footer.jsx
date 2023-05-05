407eb8e52ecf7795493daad691c3a64d555bbfdb
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-05 14:42:49 +0100

    updated link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <p className="attribution">
         Designed by
         <a className="text-indigo-950 hover:text-neutral-600" href="https://twitter.com/HeyEmdeee" target="_blank" rel="noreferrer"> Dokubo Mac-Daniel Bitebo</a>. Coded by
-        <a className="text-indigo-950 hover:text-neutral-600" href="https://github.com/IamHenryOkeke/Genri-Password-Generator" target="_blank" rel="noreferrer"> Okeke Nmesomachukwu Henry</a>.
+        <a className="text-indigo-950 hover:text-neutral-600" href="https://github.com/IamHenryOkeke/Genri-Password-Generator-Web-App" target="_blank" rel="noreferrer"> Okeke Nmesomachukwu Henry</a>.
       </p>
     </footer>
   )
