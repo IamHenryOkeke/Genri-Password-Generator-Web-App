c84f0fcdd6ef9e88e7d31d62bfd20948917513f9
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-05 14:38:53 +0100

    finish UI implementation for lading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import GeneratePasswordSection from "./components/GeneratePasswordSection"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import Description from "./components/Description"
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className="font-body">
+      <Navbar/>
+      <HeroSection/>
+      <GeneratePasswordSection/>
+      <Description/>
+      <Footer/>
+    </div>
   )
 }
 
