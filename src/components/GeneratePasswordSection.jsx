45ce726778ecd666f33c73181ee05b5106822ced
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-07 15:01:18 +0100

    added logic for user password generation

--- src/components/GeneratePasswordSection.jsx
+++ src/components/GeneratePasswordSection.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import {ReactComponent as CopyBtn} from "../assets/svg/Vector.svg";
+import useGenerate from "../../useGenerate";
 
 const GeneratePasswordSection = () => {
+  const [upperCaseValue, setUpperCaseValue] = useState(5);
+  const [lowerCaseValue, setLowerCaseValue] = useState(5);
+  const [numberValue, setNumberValue] = useState(5);
+  const [symbolValue, setSymbolValue] = useState(5);
+  const [userPassword, setUserPassword] = useState("");
+
+  const  { generatePassword } = useGenerate(upperCaseValue, lowerCaseValue, numberValue, symbolValue)
+
+  const handleOnClick = () => {
+    setUserPassword(generatePassword())
+  }
+
   return (
     <div className="px-4 md:px-0 border-indigo-950 border-b-8 py-20">
       <section
@@ -12,14 +26,17 @@ const GeneratePasswordSection = () => {
           <h4 className="md:text-3xl lg:text-4xl">Uppercase(A-Z)</h4>
           <div className="flex items-center gap-5">
             <input
+            onChange={(e) => {
+              setUpperCaseValue(e.target.value)
+            }}
               type="range"
               min="1"
               max="10"
-              value="5"
+              value={upperCaseValue}
               className="w-3/4 cursor-pointer"
             />
             <p className="flex items-center justify-center rounded-md w-[40px] h-[40px] border-4 border-indigo-950 bg-neutral-1000">
-              <span className="md:text-3xl"></span>
+              <span className="md:text-3xl">{upperCaseValue}</span>
             </p>
           </div>
         </div>
@@ -29,16 +46,19 @@ const GeneratePasswordSection = () => {
           <h4 className="md:text-3xl lg:text-4xl">Lowercase(a-z)</h4>
           <div className="flex items-center gap-5">
             <input
+              onChange={(e) => {
+                setLowerCaseValue(e.target.value)
+              }}
               type="range"
               min="1"
               max="10"
-              value="5"
+              value={lowerCaseValue}
               className="w-3/4 cursor-pointer"
             />
             <p
               className="flex items-center justify-center rounded-md w-[40px] h-[40px] border-4 border-indigo-950 bg-neutral-1000"
             >
-              <span className="md:text-3xl"></span>
+              <span className="md:text-3xl">{lowerCaseValue}</span>
             </p>
           </div>
         </div>
@@ -48,16 +68,19 @@ const GeneratePasswordSection = () => {
           <h4 className="md:text-3xl lg:text-4xl">Number(0-9)</h4>
           <div className="flex items-center gap-5">
             <input
+              onChange={(e) => {
+                setNumberValue(e.target.value)
+              }}
               type="range"
               min="1"
               max="10"
-              value="5"
+              value={numberValue}
               className="w-3/4 cursor-pointer"
             />
             <p
               className="flex items-center justify-center rounded-md w-[40px] h-[40px] border-4 border-indigo-950 bg-neutral-1000"
             >
-              <span className="md:text-3xl"></span>
+              <span className="md:text-3xl">{numberValue}</span>
             </p>
           </div>
         </div>
@@ -67,16 +90,19 @@ const GeneratePasswordSection = () => {
           <h4 className="md:text-3xl lg:text-4xl">Symbols(-@_)</h4>
           <div className="flex items-center gap-5">
             <input
+              onChange={(e) => {
+                setSymbolValue(e.target.value)
+              }}
               type="range"
               min="1"
               max="10"
-              value="5"
+              value={symbolValue}
               className="w-3/4 cursor-pointer"
             />
             <p
               className="flex items-center justify-center rounded-md w-[40px] h-[40px] border-4 border-indigo-950 bg-neutral-1000"
             >
-              <span className="md:text-3xl"></span>
+              <span className="md:text-3xl">{symbolValue}</span>
             </p>
           </div>
         </div>
@@ -85,15 +111,15 @@ const GeneratePasswordSection = () => {
         <div className="flex flex-col items-center">
           <p>Password Length:</p>
           <p className="flex items-center justify-center text-base md:text-4xl w-16 h-12 rounded-xl md:w-[150px] md:h-[60px] border-2 border-indigo-950">
-
+              {Number(upperCaseValue) + Number(lowerCaseValue)  + Number(numberValue) + Number(symbolValue)}
           </p>
         </div>
-        <button className="px-10 md:font-medium md:text-4xl md:px-20 py-2 bg-indigo-950 rounded-md hover:bg-indigo-1000">
+        <button onClick={()=>{handleOnClick()}} className="px-10 md:font-medium md:text-4xl md:px-20 py-2 bg-indigo-950 rounded-md hover:bg-indigo-1000">
           Generate
         </button>
         <div className="flex flex-col md:flex-row gap-2 items-center">
           <p className="px-3 text-sm md:text-base text-ellipsis text-center flex items-center justify-center w-[300px] h-[40px] rounded-xl md:w-[400px] md:h-[80px] border-2 border-indigo-950">
-
+              {userPassword}
           </p>
           <button className="px-4 py-[6px] flex gap-1 items-center rounded-md bg-indigo-950 md:px-4 md:py-2 hover:bg-indigo-1000">
             <CopyBtn/>
