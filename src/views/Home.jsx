f7264b4f5704fdce320b5a04d8a4839342fc73a2
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-07 16:13:18 +0100

    file restructure

---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import Description from "../components/HomeComponents/Description"
+import HeroSection from "../components/HomeComponents/HeroSection"
+import GeneratePasswordSection from "../components/HomeComponents/GeneratePasswordSection"
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <GeneratePasswordSection/>
+      <Description/>
+    </div>
+  )
+}
+
+export default Home
