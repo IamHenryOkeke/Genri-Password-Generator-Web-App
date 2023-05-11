8295ae7f295980ec8b8964fdddb65d93dfc887d7
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-11 21:22:44 +0100

    added ternery operator for saved password logic

--- src/views/SavedPassword.jsx
+++ src/views/SavedPassword.jsx
@@ -10,23 +10,31 @@ const SavedPassword = () => {
       <h2 className='text-center text-lg font-semibold'>
           Saved Passwords 
       </h2> 
-      <div className="bg-indigo-950 mt-4">
-        {
-          savedPasswords.map((index) => (
-            <div key={index} className="flex flex-col gap-3 py-4 mx-4 md:mx-8">
-              <div className="p-4 bg-indigo-1000 rounded-2xl">
-                <h2 className='mb-4'>
-                  Site: {JSON.parse(localStorage.getItem(index)).site}
-                </h2>
-                <p>
-                  Password: {JSON.parse(localStorage.getItem(index)).password}
-                </p>
+      {
+        savedPasswords.length >= 1 ? (
+          <div className="bg-indigo-950 mt-4">
+          {
+            savedPasswords.map((index) => (
+              <div key={index} className="flex flex-col gap-3 py-4 mx-4 md:mx-8">
+                <div className="p-4 bg-indigo-1000 rounded-2xl">
+                  <h2 className='mb-4'>
+                    Site: {JSON.parse(localStorage.getItem(index)).site}
+                  </h2>
+                  <p>
+                    Password: {JSON.parse(localStorage.getItem(index)).password}
+                  </p>
+                </div>
+                
               </div>
-              
-            </div>
-          ))
-        }
-      </div> 
+            ))
+          }
+        </div> 
+        ) : (
+            <p className='text-center'>
+              No Passwords Founds
+            </p>
+        )
+      }
       
     </div>
   )
