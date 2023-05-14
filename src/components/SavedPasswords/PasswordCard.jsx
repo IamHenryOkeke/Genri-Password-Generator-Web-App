099bf8f771466fdecf051d85a0803b7a3a4b8af8
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-14 18:14:07 +0100

    fixed ui responsiveness

--- src/components/SavedPasswords/PasswordCard.jsx
+++ src/components/SavedPasswords/PasswordCard.jsx
@@ -9,15 +9,15 @@ const PasswordCard = ({site, password}) => {
     copyFunc(password);
   }
   return (
-    <div className="p-4 bg-indigo-1000 rounded-2xl">
+    <div className="p-4 bg-indigo-1000 rounded-2xl lg:w-1/2">
         <h2 className='mb-4'>
-            {site}
+            {site} 
         </h2>
         <div className='flex items-center justify-between'>
             <p className="flex items-center py-4 pl-2 rounded w-9/12 h-5 text-base border-2 border-indigo-950 bg-neutral-1000">
                 {password}
             </p>
-            <button onClick={() => {handleCopyClick()}} className="px-4 text-[10px] py-2 flex gap-1 items-center rounded-xl bg-indigo-950 md:px-4 md:py-2 hover:bg-indigo-1000">
+            <button onClick={() => {handleCopyClick()}} className="px-4 text-[10px] lg:text-base py-2 flex gap-1 items-center rounded-xl bg-indigo-950 md:px-4 md:py-2 hover:bg-indigo-1000">
                 <CopyBtn className="fill-black"/>
                 <p>Copy</p>
             </button>
@@ -30,7 +30,6 @@ const PasswordCard = ({site, password}) => {
 PasswordCard.propTypes = {
     site: PropTypes.string.isRequired,
     password : PropTypes.string.isRequired,
-    handleCopy : PropTypes.func.isRequired
 }
 
 export default PasswordCard
