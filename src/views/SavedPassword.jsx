ac803bd0431b1ca233608dfdfb04f49ce48c526c
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-14 18:08:09 +0100

    fixed ui responsiveness

--- src/views/SavedPassword.jsx
+++ src/views/SavedPassword.jsx
@@ -16,7 +16,7 @@ const SavedPassword = () => {
           <div className="bg-indigo-950 mt-4 rounded-xl mx-4 md:mx-0 md:rounded-none">
           {
             savedPasswords.map((index) => (
-              <div key={index} className="flex flex-col gap-3 py-4 mx-4 md:mx-8 lg:mx-32">
+              <div key={index} className="flex flex-col lg:items-center gap-3 py-4 mx-4 md:mx-8 lg:mx-32">
                 <PasswordCard site={JSON.parse(localStorage.getItem(index)).site} password={JSON.parse(localStorage.getItem(index)).password}  />
               </div>
             ))
