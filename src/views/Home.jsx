520fd808b74c2c75c110bb2870d30b0a8fe7e4ac
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-28 21:10:39 +0100

    greetings rendering

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,33 @@
-import Description from "../components/HomeComponents/Description"
-import HeroSection from "../components/HomeComponents/HeroSection"
-import GeneratePasswordSection from "../components/HomeComponents/GeneratePasswordSection"
+import Description from "../components/HomeComponents/Description";
+import HeroSection from "../components/HomeComponents/HeroSection";
+import GeneratePasswordSection from "../components/HomeComponents/GeneratePasswordSection";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Home = () => {
+  const [userStatus, setUserStatus] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in
+            setUserStatus(user);
+        } else {
+            // User is signed out
+            setUserStatus(null);
+        }
+    })
+  }, [])
   return (
-    <div  className="font-body">
+    <div className="font-body">
+      {
+        userStatus && <p className="mx-4 md:mx-8 lg:mx-32 text-lg md:text-4xl font-semibold mt-4">
+          Welcome{" "}
+          {userStatus.displayName}{" "}
+          &#128075;
+        </p>
+      }
       <HeroSection/>
       <GeneratePasswordSection/>
       <Description/>
